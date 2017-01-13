124b0189e99c61304ca1b661231d7269dc15394c
renxw <rolandxwren@gmail.com>
2017-01-14 00:39:30 +0800

    1. (-)修改: 生成guid出错的问题.

--- gen.py
+++ gen.py
@@ -38,7 +38,7 @@ def indent_down():
     indent -= 2
 
 def gen_GUID():
-    op = subprocess.Popen(['.\\GuidGen.exe', '/u'], stdout=subprocess.PIPE)
+    op = subprocess.Popen(['.\\GuidGen.exe', '/u', '/nocopy'], stdout=subprocess.PIPE)
     guidstr, _ = op.communicate()
     guidret = str(guidstr)[2:-5]
     print('gen_GUID ' + guidret)
