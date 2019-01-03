99a804ce5d79ac3bcb4aa0f5081a917d4df1fccc
rollandyim <rollandyim@gmail.com>
2019-01-03 21:58:43 +0800

    1. (-)Fixed bug: Can't generate from Chinease file dirname or filename.

--- gen.py
+++ gen.py
@@ -1,26 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file: gen.py
+@time: 2019-1-3 21:25:57
+@author: rollandyim
+@email: rollandyim@gmail.com
+"""
+
 import os
 import subprocess
 import sys
 
-c_include_exts = ['.c', '.cpp', '.cc', '.cxx', '.h', '.hpp', '.hxx', '.inl', '.inc']
+c_include_exts = ['.h', '.hpp', '.hxx']
+c_source_exts = ['.c', '.cpp', '.cc', '.cxx', '.inl', '.inc']
+
 
 def visit(folder):
-    global files
     global folders
-    for fn in os.listdir(folder):
-        filename = os.path.join(folder, fn)
-        _, ext = os.path.splitext(fn)
-        if os.path.isdir(filename):
-            folders.append(filename)
-            visit(filename)
-        elif ext.lower() in c_include_exts:
-            files.append(filename)
+    for root, dirnames, filenames in os.walk(folder):
+        for d in dirnames:
+            full_path = os.path.join(root, d)
+            #print full_path, " is0 ", isinstance(full_path, str)
+            full_path = unicode(full_path, 'gbk')
+            #print full_path, " is1 ", isinstance(full_path, unicode)
+            if '\\.' not in full_path and '/.' not in full_path:
+                folders.append(full_path)
+        for f in filenames:
+            full_path = os.path.join(root, f)
+            #print full_path, " file0 ", isinstance(full_path, str)
+            full_path = unicode(full_path, 'gbk')
+            #print full_path, " file1 ", isinstance(full_path, unicode)
+            isinstance(full_path, str)
+            _, ext = os.path.splitext(f)
+            if ext in c_include_exts:
+                include_files.append(full_path)
+            elif ext in c_source_exts:
+                source_files.append(full_path)
 
-files = []
+
+include_files = []
+source_files = []
 folders = []
 indent = 0
 proj_guid = ''
 vcxproj_guid = ''
+
 
 def indent_fill():
     global indent
@@ -29,13 +53,16 @@ def indent_fill():
         str += ' '
     return str
 
+
 def indent_up():
     global indent
     indent += 2
 
+
 def indent_down():
     global indent
     indent -= 2
+
 
 def gen_GUID():
     curdir = os.path.dirname(os.path.realpath(__file__))
@@ -45,11 +72,13 @@ def gen_GUID():
     print('gen_GUID ' + guidret)
     return guidret
 
+
 def get_proj_guid():
     global proj_guid
     if (len(proj_guid) < 1):
         proj_guid = gen_GUID()
     return proj_guid
+
 
 def get_vcxproj_guid():
     global vcxproj_guid
@@ -57,13 +86,13 @@ def get_vcxproj_guid():
         vcxproj_guid = gen_GUID()
     return vcxproj_guid
 
-def generate_vcxproj(project_name, code_folder, save_path):
-    global files
 
-    file = open(save_path, 'w')
+def generate_vcxproj(project_name, code_folder, save_path):
+    file = open(save_path, 'wb')
 
     file.write('<?xml version="1.0" encoding="utf-8"?>\r\n')
-    file.write('<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">\r\n')
+    file.write(
+        '<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">\r\n')
 
     indent_up()
     file.write(indent_fill() + '<ItemGroup Label="ProjectConfigurations">\r\n')
@@ -99,7 +128,8 @@ def generate_vcxproj(project_name, code_folder, save_path):
     indent_down()
 
     indent_up()
-    file.write(indent_fill() + '<PropertyGroup Condition="\'$(Configuration)|$(Platform)\'==\'Debug|Win32\'" Label="Configuration">\r\n')
+    file.write(
+        indent_fill() + '<PropertyGroup Condition="\'$(Configuration)|$(Platform)\'==\'Debug|Win32\'" Label="Configuration">\r\n')
     indent_up()
     file.write(indent_fill() + '<ConfigurationType>Application</ConfigurationType>\r\n')
     file.write(indent_fill() + '<UseDebugLibraries>true</UseDebugLibraries>\r\n')
@@ -109,7 +139,8 @@ def generate_vcxproj(project_name, code_folder, save_path):
     indent_down()
 
     indent_up()
-    file.write(indent_fill() + '<PropertyGroup Condition="\'$(Configuration)|$(Platform)\'==\'Release|Win32\'" Label="Configuration">\r\n')
+    file.write(
+        indent_fill() + '<PropertyGroup Condition="\'$(Configuration)|$(Platform)\'==\'Release|Win32\'" Label="Configuration">\r\n')
     indent_up()
     file.write(indent_fill() + '<ConfigurationType>Application</ConfigurationType>\r\n')
     file.write(indent_fill() + '<UseDebugLibraries>true</UseDebugLibraries>\r\n')
@@ -125,17 +156,21 @@ def generate_vcxproj(project_name, code_folder, save_path):
     indent_down()
 
     indent_up()
-    file.write(indent_fill() + '<ImportGroup Label="PropertySheets" Condition="\'$(Configuration)|$(Platform)\'==\'Debug|Win32\'">\r\n')
+    file.write(
+        indent_fill() + '<ImportGroup Label="PropertySheets" Condition="\'$(Configuration)|$(Platform)\'==\'Debug|Win32\'">\r\n')
     indent_up()
-    file.write(indent_fill() + '<Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists(\'$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props\')" Label="LocalAppDataPlatform" />\r\n')
+    file.write(
+        indent_fill() + '<Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists(\'$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props\')" Label="LocalAppDataPlatform" />\r\n')
     indent_down()
     file.write(indent_fill() + '</ImportGroup>\r\n')
     indent_down()
 
     indent_up()
-    file.write(indent_fill() + '<ImportGroup Label="PropertySheets" Condition="\'$(Configuration)|$(Platform)\'==\'Release|Win32\'">\r\n')
+    file.write(
+        indent_fill() + '<ImportGroup Label="PropertySheets" Condition="\'$(Configuration)|$(Platform)\'==\'Release|Win32\'">\r\n')
     indent_up()
-    file.write(indent_fill() + '<Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists(\'$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props\')" Label="LocalAppDataPlatform" />\r\n')
+    file.write(
+        indent_fill() + '<Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists(\'$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props\')" Label="LocalAppDataPlatform" />\r\n')
     indent_down()
     file.write(indent_fill() + '</ImportGroup>\r\n')
     indent_down()
@@ -161,7 +196,8 @@ def generate_vcxproj(project_name, code_folder, save_path):
     indent_down()
 
     indent_up()
-    file.write(indent_fill() + '<ItemDefinitionGroup Condition="\'$(Configuration)|$(Platform)\'==\'Debug|Win32\'">\r\n')
+    file.write(
+        indent_fill() + '<ItemDefinitionGroup Condition="\'$(Configuration)|$(Platform)\'==\'Debug|Win32\'">\r\n')
     indent_up()
     file.write(indent_fill() + '<ClCompile>\r\n')
     indent_up()
@@ -169,7 +205,8 @@ def generate_vcxproj(project_name, code_folder, save_path):
     file.write(indent_fill() + '</PrecompiledHeader>\r\n')
     file.write(indent_fill() + '<WarningLevel>Level3</WarningLevel>\r\n')
     file.write(indent_fill() + '<Optimization>Disabled</Optimization>\r\n')
-    file.write(indent_fill() + '<PreprocessorDefinitions>WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions)</PreprocessorDefinitions>\r\n')
+    file.write(
+        indent_fill() + '<PreprocessorDefinitions>WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions)</PreprocessorDefinitions>\r\n')
     indent_down()
     file.write(indent_fill() + '</ClCompile>\r\n')
     file.write(indent_fill() + '<Link>\r\n')
@@ -183,7 +220,8 @@ def generate_vcxproj(project_name, code_folder, save_path):
     indent_down()
 
     indent_up()
-    file.write(indent_fill() + '<ItemDefinitionGroup Condition="\'$(Configuration)|$(Platform)\'==\'Release|Win32\'">\r\n')
+    file.write(
+        indent_fill() + '<ItemDefinitionGroup Condition="\'$(Configuration)|$(Platform)\'==\'Release|Win32\'">\r\n')
     indent_up()
     file.write(indent_fill() + '<ClCompile>\r\n')
     indent_up()
@@ -191,7 +229,8 @@ def generate_vcxproj(project_name, code_folder, save_path):
     file.write(indent_fill() + '</PrecompiledHeader>\r\n')
     file.write(indent_fill() + '<WarningLevel>Level3</WarningLevel>\r\n')
     file.write(indent_fill() + '<Optimization>Disabled</Optimization>\r\n')
-    file.write(indent_fill() + '<PreprocessorDefinitions>WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions)</PreprocessorDefinitions>\r\n')
+    file.write(
+        indent_fill() + '<PreprocessorDefinitions>WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions)</PreprocessorDefinitions>\r\n')
     indent_down()
     file.write(indent_fill() + '</ClCompile>\r\n')
     file.write(indent_fill() + '<Link>\r\n')
@@ -207,10 +246,19 @@ def generate_vcxproj(project_name, code_folder, save_path):
     indent_up()
     file.write(indent_fill() + '<ItemGroup>\r\n')
     indent_up()
-    for fn in files:
-        diff_len = len(fn) - len(code_folder)
-        rel_fn = fn[-diff_len:]
-        file.write(indent_fill() + '<ClCompile Include="' + rel_fn + '"/>\r\n')
+    for fn in include_files:
+        rel_fn = fn.lstrip(code_folder)
+        file.write(indent_fill() + '<ClInclude Include="' + rel_fn.encode("utf-8") + '"/>\r\n')
+    indent_down()
+    file.write(indent_fill() + '</ItemGroup>\r\n')
+    indent_down()
+
+    indent_up()
+    file.write(indent_fill() + '<ItemGroup>\r\n')
+    indent_up()
+    for fn in source_files:
+        rel_fn = fn.lstrip(code_folder)
+        file.write(indent_fill() + '<ClCompile Include="' + rel_fn.encode("utf-8") + '"/>\r\n')
     indent_down()
     file.write(indent_fill() + '</ItemGroup>\r\n')
     indent_down()
@@ -227,11 +275,11 @@ def generate_vcxproj(project_name, code_folder, save_path):
 
     print('generate %s ok' % save_path)
 
+
 def generate_vcxproj_filters(project_name, code_folder, save_path):
-    global files
     global folders
 
-    file = open(save_path, 'w')
+    file = open(save_path, 'wb')
 
     file.write('<?xml version="1.0" encoding="utf-8"?>\r\n')
     file.write('<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">\r\n')
@@ -240,9 +288,9 @@ def generate_vcxproj_filters(project_name, code_folder, save_path):
     file.write(indent_fill() + '<ItemGroup>\r\n')
     indent_up()
     for fn in folders:
-        diff_len = len(fn) - len(code_folder)
-        rel_fn = fn[-diff_len:]
-        file.write(indent_fill() + '<Filter Include="' + rel_fn + '"/>\r\n')
+        rel_fn = fn.lstrip(code_folder)
+        if rel_fn != "":
+            file.write(indent_fill() + '  <Filter Include="' + rel_fn.encode("utf-8") + '"/>\r\n')
     indent_down()
     file.write(indent_fill() + '</ItemGroup>\r\n')
     indent_down()
@@ -250,30 +298,45 @@ def generate_vcxproj_filters(project_name, code_folder, save_path):
     indent_up()
     file.write(indent_fill() + '<ItemGroup>\r\n')
     indent_up()
-    for fn in files:
-        diff_len = len(fn) - len(code_folder)
-        rel_fn = fn[-diff_len:]
-        rel_dir, _ = os.path.split(rel_fn)
-        rel_dir.rstrip('\\')
-        file.write(indent_fill() + '<ClCompile Include="' + rel_fn + '">\r\n')
-        if len(rel_dir) > 0:
-            file.write('  <Filter>' + rel_dir + '</Filter>\r\n')
+    for fn in include_files:
+        rel_fn = fn.lstrip(code_folder)
+        rel_dir = os.path.dirname(rel_fn)
+        file.write(indent_fill() + '<ClInclude Include="' + rel_fn.encode("utf-8") + '">\r\n')
+        if rel_fn != "":
+            file.write(indent_fill() + '  <Filter>' + rel_dir.encode("utf-8") + '</Filter>\r\n')
+        file.write(indent_fill() + '</ClInclude>\r\n')
+    indent_down()
+    file.write(indent_fill() + '</ItemGroup>\r\n')
+    indent_down()
+
+    indent_up()
+    file.write(indent_fill() + '<ItemGroup>\r\n')
+    indent_up()
+    for fn in source_files:
+        rel_fn = fn.lstrip(code_folder)
+        rel_dir = os.path.dirname(rel_fn)
+        file.write(indent_fill() + '<ClCompile Include="' + rel_fn.encode("utf-8") + '">\r\n')
+        if rel_fn != "":
+            file.write(indent_fill() + '  <Filter>' + rel_dir.encode("utf-8") + '</Filter>\r\n')
         file.write(indent_fill() + '</ClCompile>\r\n')
     indent_down()
     file.write(indent_fill() + '</ItemGroup>\r\n')
     indent_down()
+
     file.write('</Project>\r\n')
 
     file.close()
 
     print('generate %s ok' % save_path)
 
+
 def generate_sln(project_name, code_folder, save_path):
-    file = open(save_path, 'w')
+    file = open(save_path, 'wb')
 
     file.write('Microsoft Visual Studio Solution File, Format Version 11.00\r\n')
     file.write('# Visual Studio 2010\r\n')
-    file.write('Project("{' + get_proj_guid() + '}") = "' + project_name + '", "' + project_name + '.vcxproj", "{' + get_vcxproj_guid() + '}"\r\n')
+    file.write(
+        'Project("{' + get_proj_guid() + '}") = "' + project_name + '", "' + project_name + '.vcxproj", "{' + get_vcxproj_guid() + '}"\r\n')
     file.write('EndProject\r\n')
     file.write('Global\r\n')
 
@@ -305,6 +368,7 @@ def generate_sln(project_name, code_folder, save_path):
     file.close()
 
     print('generate %s ok' % save_path)
+
 
 def main(argv):
     if (len(argv) < 2):
@@ -345,6 +409,7 @@ def main(argv):
     generate_vcxproj(proj_name, code_folder, vcx_path)
     generate_vcxproj_filters(proj_name, code_folder, vcx_filter_path)
     generate_sln(proj_name, code_folder, sln_path)
+
 
 if __name__ == "__main__":
     main(sys.argv)
